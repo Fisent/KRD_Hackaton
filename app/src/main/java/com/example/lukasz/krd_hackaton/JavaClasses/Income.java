8d5333f145ce23ab63e659bd69fcd6a191e6203f
juna8001 <juna8001@gmail.com>
2017-05-20 18:17:59 +0200

    Jebać Policję na 99%!!!

--- app/src/main/java/com/example/lukasz/krd_hackaton/JavaClasses/Income.java
+++ app/src/main/java/com/example/lukasz/krd_hackaton/JavaClasses/Income.java
@@ -4,6 +4,11 @@ import java.util.Date;
 
 public class Income
 {
+    public Income(Date date, double value){
+        this.date = date;
+        this.value = value;
+    }
+
     public Date date;
     public double value;
 }
