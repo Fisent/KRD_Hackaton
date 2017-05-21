404ebaa226db29bab71e27c9b6f271f29d62bd1a
juna8001 <juna8001@gmail.com>
2017-05-21 05:59:15 +0200

    jebać biedę

--- app/src/main/java/com/example/lukasz/krd_hackaton/MySpecialAdapter.java
+++ app/src/main/java/com/example/lukasz/krd_hackaton/MySpecialAdapter.java
@@ -5,9 +5,11 @@ import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.lukasz.krd_hackaton.JavaClasses.Base;
 import com.example.lukasz.krd_hackaton.JavaClasses.Debt;
@@ -34,8 +36,10 @@ public class MySpecialAdapter extends ArrayAdapter<Debt>
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
     {
         Debt debt = debts.get(position);
-        parent.findViewById(R.id.text1);
+        TextView view = (TextView) parent.findViewById(R.id.text1);
         //TUTAJ PISZ
+
+        view.setText(Html.fromHtml(debt.toString()));
 
         return super.getView(position, convertView, parent);
     }
